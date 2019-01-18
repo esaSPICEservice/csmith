0fff4b397a5fb0284164e5b4b6c7abbdb6d29d9a
Marc Costa <marc.costa@sciops.esa.int>
2019-01-18 13:59:31 +0100

    Updates with A.Escalante

--- spirec/core/solar_matrix.py
+++ spirec/core/solar_matrix.py
@@ -9,32 +9,36 @@ import imageio
 from scipy.misc import imsave
 
 
-def plot_solar_ilum(utc, metakernel, sensor, target, target_frame,
-                    observer, pixel_lines=False, pixel_samples=False, dsk=False,
-                    generate_image=False):
+def plot_solar_ilum(utc, metakernel, camera, target, target_frame,
+                    pixel_lines=False, pixel_samples=False, dsk=False,
+                    generate_image=False, report=False):
     '''
 
-    :param solarmax:  Determination of this value is dependent on
-    each image and
-    :type solarmax:
-    :param utc:
-    :type utc:
-    :param metakernel:
-    :type metakernel:
-    :param sensor:
-    :type sensor:
-    :param target:
-    :type target:
-    :param target_frame:
-    :type target_frame:
-    :param observer:
-    :type observer:
-    :param pixel_lines:
-    :type pixel_lines:
-    :param pixel_samples:
-    :type pixel_samples:
-    :param dsk:
-    :type dsk:
+    :param utc: Image aquisition time in UTC format e.g.: 2016-01-01T00:00:00
+    :type utc: str
+    :param metakernel: SPICE Kernel Dataset Meta-Kernel
+    :type metakernel: str
+    :param camera: Name of the camera to be used. Usually found in the
+    instrument kernel (IK) e.g.: 'ROS_NAVCAM-A'
+    :type camera: str
+    :param target: Target of the observation, e.g.:'67P/C-G'
+    :type target: str
+    :param target_frame: SPICE reference frame of the target. Usually found in
+    the frames kernel (FK)
+    :type target_frame: str
+    :param pixel_lines: Number of pixel lines usually provided by the IK.
+    :type pixel_lines: int
+    :param pixel_samples: Number of pixel samples per line usually provided by
+    the IK.
+    :type pixel_samples: int
+    :param dsk: Digital Shape Model to be used for the computation. Not required
+    of included in the Meta-Kernel.
+    :type dsk: str
+    :param generate_image: Flag to determine whether if the image is saved or
+    plotted.
+    :type generate_image: bool
+    :param report: Flag for processing repor.
+    :type generate_image: bool
     :return:
     :rtype:
     '''
@@ -44,6 +48,9 @@ def plot_solar_ilum(utc, metakernel, sensor, target, target_frame,
 
     if dsk:
         spiceypy.furnsh(dsk)
+        method = 'DSK/UNPRIORITIZED'
+    else:
+        method = 'ELLIPSOID'
 
     et = spiceypy.utc2et(utc)
 
@@ -53,26 +60,39 @@ def plot_solar_ilum(utc, metakernel, sensor, target, target_frame,
     mpl.rcParams['figure.figsize'] = (26.0, 26.0)
 
     #
-    # We retrieve the sensor information using GETFOV. More info available:
+    # We retrieve the camera information using GETFOV. More info available:
     #
     #   https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/getfov_c.html
     #
-    sensor_name = sensor
-    sensor_id = spiceypy.bodn2c(sensor_name)
-    (shape, frame, bsight, vectors, bounds) = spiceypy.getfov(sensor_id, 100)
+    camera_name = camera
+    camera_id = spiceypy.bodn2c(camera_name)
+    (shape, frame, bsight, vectors, bounds) = spiceypy.getfov(camera_id, 100)
+
 
     #
-    # We check if the resolution of the sensor has been provided as an input
-    # if not we try to obtain the resolution of the sensor from the IK
+    # TODO: In the future all the sensors should be epehmeris objects, see
+    # https://issues.cosmos.esa.int/socci/browse/SPICEMNGT-77
+    #
+    if camera.split('_')[0] == 'ROS':
+        observer = 'ROSETTA'
+    elif camera.split('_')[0] == 'MEX':
+        observer = 'MEX'
+    elif camera.split('_')[0] == 'VEX':
+        observer = 'VEX'
+
+
+    #
+    # We check if the resolution of the camera has been provided as an input
+    # if not we try to obtain the resolution of the camera from the IK
     #
     if not pixel_lines or not pixel_samples:
         try:
-            pixel_samples = int(spiceypy.gdpool('INS'+str(sensor_id) + '_PIXEL_SAMPLES',0,1))
-            pixel_lines = int(spiceypy.gdpool('INS' + str(sensor_id) + '_PIXEL_LINES',0,1))
+            pixel_samples = int(spiceypy.gdpool('INS'+str(camera_id) + '_PIXEL_SAMPLES',0,1))
+            pixel_lines = int(spiceypy.gdpool('INS' + str(camera_id) + '_PIXEL_LINES',0,1))
         except:
             pass
             print("PIXEL_SAMPLES and/or PIXEL_LINES not defined for "
-                  "{}".format(sensor))
+                  "{}".format(camera))
             return
 
     #
@@ -110,22 +130,22 @@ def plot_solar_ilum(utc, metakernel, sensor, target, target_frame,
             ibsight = [x[i], y[j], bsight[2]]
 
             try:
-                (spoint, trgepc, srfvec ) = spiceypy.sincpt('DSK/UNPRIORITIZED',
-                                            target, et, target_frame, 'NONE', observer, frame, ibsight)
+                (spoint, trgepc, srfvec ) = spiceypy.sincpt(method, target, et,
+                                            target_frame, 'NONE', observer, frame, ibsight)
                 (trgepc, srfvec, phase, solar,
-                 emissn, visiblef, iluminatedf) = spiceypy.illumf('DSK/UNPRIORITIZED',
-                                            target, 'SUN', et, target_frame, 'LT+S', observer, spoint)
+                 emissn, visiblef, iluminatedf) = spiceypy.illumf(method, target, 'SUN', et,
+                                                  target_frame, 'LT+S', observer, spoint)
 
                 emissn_matrix[i, j] = emissn
                 phase_matrix[i, j] = phase
 
                 #
-                # Add to list if the point is visible to the sensor
+                # Add to list if the point is visible to the camera
                 #
                 if visiblef == True:
                     isvisible.append(visiblef)
                 #
-                # Add to list if the point is illuminated and seen by the sensor
+                # Add to list if the point is illuminated and seen by the camera
                 #
                 if iluminatedf == True:
 
@@ -149,29 +169,30 @@ def plot_solar_ilum(utc, metakernel, sensor, target, target_frame,
                 phase_matrix[i,j] = np.pi
                 solar_matrix[i,j] = np.pi/2
 
-    print('Pixel report for {} w.r.t {} @ {}'.format(sensor,target,utc))
-    print('   Total number of pixels: ', pixel_samples*pixel_lines)
-    print('   Illuminated pixels:     ', len(isvisible))
-    print('   Hidden pixels:          ', pixel_samples*pixel_lines - len(isvisible))
-    print('   Shadowed points:        ', pixel_samples*pixel_lines - len(isiluminated))
+    if report:
+        print('Pixel report for {} w.r.t {} @ {}'.format(camera,target,utc))
+        print('   Total number of pixels: ', pixel_samples*pixel_lines)
+        print('   Illuminated pixels:     ', len(isvisible))
+        print('   Hidden pixels:          ', pixel_samples*pixel_lines - len(isvisible))
+        print('   Shadowed points:        ', pixel_samples*pixel_lines - len(isiluminated))
 
     #
     # We transform the matrix from illumination angles to greyscale [0-255]
     #
-    rescaled = (255 / solar_matrix.max() * (solar_matrix - solar_matrix.min())).astype(np.uint8)
+    rescaled = (255 / (solar_matrix.max()-solar_matrix.min()) * (solar_matrix - solar_matrix.min())).astype(np.uint8)
     rescaled = - np.flip(rescaled, 0) + 255
-    rescaled -= np.amin(rescaled)
 
     #
     # We generate the plot
     #
     if generate_image:
-        name = '{}_{}_{}.png'.format(sensor.lower(),
+        name = '{}_{}_{}.png'.format(camera.lower(),
                                      dsk.split('/')[-1].split('.')[0].lower(),
                                      utc.lower())
         imageio.imwrite(name, rescaled)
     else:
         plt.imshow(rescaled, cmap='gray')
+        plt.axis('off')
         plt.show()
 
     return
@@ -179,19 +200,25 @@ def plot_solar_ilum(utc, metakernel, sensor, target, target_frame,
 
 target = 'PHOBOS'
 target_frame = 'IAU_PHOBOS'
-observer = 'MEX'
-sensor = 'MEX_HRSC_SRC'
+camera = 'MEX_HRSC_SRC'
 metakernel = '/Users/mcosta/MARS-EXPRESS/kernels/mk/MEX_OPS_LOCAL.TM'
 dsk = '/Users/mcosta/MARS-EXPRESS/kernels/dsk/PHOBOS_K137_DLR_V01.BDS'
 utc = '2010-08-27T20:31:56'
 
 target = '67P/C-G'
 target_frame = '67P/C-G_CK'
-observer = 'ROSETTA'
-sensor = 'ROS_NAVCAM-A'
+camera = 'ROS_NAVCAM-A'
 metakernel = '/Users/mcosta/ROSETTA/kernels/mk/ROS_OPS_LOCAL.TM'
 dsk = '/Users/mcosta/ROSETTA/kernels/dsk/ROS_CG_M001_OSPCLPS_N_V1.BDS'
 utc = '2016-02-24T14:53:39'
 
-plot_solar_ilum(utc=utc, metakernel=metakernel, sensor=sensor, target=target, target_frame=target_frame, observer=observer, dsk=dsk,
-                generate_image=True, pixel_lines=100, pixel_samples=100)
+target = '67P/C-G'
+target_frame = '67P/C-G_CK'
+camera = 'ROS_OSIRIS_WAC_DIST'
+metakernel = '/Users/mcosta/ROSETTA/kernels/mk/ROS_OPS_LOCAL.TM'
+dsk = '/Users/mcosta/ROSETTA/kernels/dsk/ROS_CG_M001_OSPCLPS_N_V1.BDS'
+utc = '2016-02-24T14:53:39'
+
+
+plot_solar_ilum(utc=utc, metakernel=metakernel, camera=camera, target=target, target_frame=target_frame, dsk=dsk,
+                pixel_lines=100, pixel_samples=100, generate_image=True, report=True)
