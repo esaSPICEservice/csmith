9f76d35bb2efc1ef9715d73eaad75a0f75de8d4a
Marc Costa <marc.costa@sciops.esa.int>
2019-02-12 22:08:38 +0100

    Working updates, pipeline orchestrated.

--- spirec/spirec.py
+++ spirec/spirec.py
@@ -18,33 +18,35 @@ def spirec(config):
     with open('quaternions.txt', 'w+') as f:
         for image in images:
 
-            (utc, target) = read_pds_label(config['labels_dir']+os.sep+image.split('.')[0] + '.LBL')
+            try:
+                (utc, target) = read_pds_label(config['labels_dir']+os.sep+image.split('.')[0] + '.LBL')
 
-            if target == '67P/CHURYUMOV-GERASIMENKO 1 (1969 R1)':
+                if target == '67P/CHURYUMOV-GERASIMENKO 1 (1969 R1)':
 
-                simulation = image.split('.')[0] + '_SIMULATION.PNG'
-                if not simulation in simulations:
+                    simulation = image.split('.')[0] + '_SIMULATION.PNG'
+                    if not simulation in simulations:
 
-                    simulation = simulate_image(utc=utc, metakernel=config['mk'],
-                       camera=config['camera'], target=config['target'],
-                       target_frame=config['target_frame'],
-                       dsk=config['dsk'], generate_image=True, report=True,
-                       name=image.split('.')[0]+'_SIMULATION.PNG')
+                        simulation = simulate_image(utc=utc, metakernel=config['mk'],
+                            camera=config['camera'], target=config['target'],
+                            target_frame=config['target_frame'],
+                            dsk=config['dsk'], generate_image=True, report=True,
+                            name=image.split('.')[0]+'_SIMULATION.PNG')
 
-                shutil.copy(config['images_dir']+os.sep+image, image)
+                    shutil.copy(config['images_dir']+os.sep+image, image)
 
-                plane = process_image.ilum_bin(image)
-                plane_f = process_image.ilum_bin(simulation)
+                    plane = process_image.ilum_bin(image)
+                    plane_f = process_image.ilum_bin(simulation)
 
-                corrections = process_image.opt_fit(plane, plane_f, image)
+                    corrections = process_image.opt_fit(plane, plane_f, image)
 
-                quaternion = generate_quaternion(corrections)
+                    quaternion = generate_quaternion(corrections)
 
-                f.write('{} {} {} {} {}\n'.format(utc, quaternion[0],
-                                                quaternion[1],
-                                                quaternion[2],
-                                                quaternion[3]))
-
+                    f.write('{} {} {} {} {}\n'.format(utc, quaternion[0],
+                                                           quaternion[1],
+                                                           quaternion[2],
+                                                           quaternion[3]))
+            except:
+                print('{} not processed'.format(image))
 
     return
 
